9bd9265ed9e5aae9bfce39024d2a3f1b6d8af888
Marco Galizzi <marcogalizzi89@gmail.com>
2018-10-15 16:34:09 +0200

    feat: added mock hash data for user, customer, coffee. if hash coming from bockchain match hash coming from frontend the document is CONFIRMED

--- demux/updaters/coffee/upsert.coffee.js
+++ demux/updaters/coffee/upsert.coffee.js
@@ -1,10 +1,49 @@
+const md5 = require('md5');
+const Coffee = require('../../../schemas/coffee');
+
 async function upsertCoffee (state, payload, blockInfo, context) {
   try {
     const { owner, uuid, hash, price, quantity } = payload.data;
     //eslint-disable-next-line
-    console.log(`Upsert coffee: owner: ${owner} | uuid: ${uuid} | hash: ${hash} | price: ${price} | quantity: ${quantity}`);
+    console.log(
+      `Upsert coffee: owner: ${owner} | uuid: ${uuid} | hash: ${hash} | price: ${price} | quantity: ${quantity}`
+    );
     // TODO: check information with database and update/insert coffee
     // and switch from pending to confirmed as appropriate
+    const foundCoffee = await Coffee.find({
+      where: { id: uuid }
+    });
+    const {
+      name,
+      botanical_variety,
+      preparation,
+      altitude,
+      region,
+      roast_appearance,
+      details,
+      price_kg,
+      geo_location
+    } = foundCoffee.dataValues;
+    const bodyToCheck = JSON.stringify({
+      name,
+      botanical_variety,
+      preparation,
+      altitude,
+      region,
+      roast_appearance,
+      details,
+      price_kg,
+      geo_location
+    });
+    if (hash === md5(bodyToCheck)) {
+      Coffee.update(
+        { confirmed: true },
+        { returning: true, plain: true, where: { id: uuid } }
+      );
+      console.log('CONFIRMED');
+    } else {
+      console.log('NOT CONFIRMED: ', hash, md5(bodyToCheck));
+    }
   } catch (err) {
     //eslint-disable-next-line
     console.error(err);
--- demux/updaters/user/upsert.user.js
+++ demux/updaters/user/upsert.user.js
@@ -1,10 +1,68 @@
+const md5 = require('md5');
+const Customer = require('../../../schemas/customer');
+const Producer = require('../../../schemas/producer');
+
 async function upsertUser (state, payload, blockInfo, context) {
   try {
     const { user, role, hash } = payload.data;
+    let foundUser;
     //eslint-disable-next-line
     console.log(`Upsert user: user: ${user} | role: ${role} | hash: ${hash}`);
     // TODO: check information with database and update/insert user
     // and switch from pending to confirmed as appropriate
+    if (role === 'consumer') {
+      foundUser = await Customer.find({
+        where: { customer_name: user }
+      });
+      const {
+        customer_name,
+        country,
+        description,
+        geo_location,
+        email
+      } = foundUser.dataValues;
+      const bodyToCheck = JSON.stringify({
+        customer_name,
+        country,
+        geo_location,
+        description,
+        email
+      });
+      if (hash === md5(bodyToCheck)) {
+        Customer.update(
+          { confirmed: true },
+          { returning: true, plain: true, where: { customer_name: user } }
+        );
+        console.log('CONFIRMED');
+      } else {
+        console.log('NOT CONFIRMED: ', hash, md5(bodyToCheck));
+      }
+    } else if (role === 'producer') {
+      foundUser = await Producer.find({
+        where: { business_name: user }
+      });
+      const {
+        business_name,
+        country,
+        description,
+        email
+      } = foundUser.dataValues;
+      const bodyToCheck = JSON.stringify({
+        business_name,
+        country,
+        description,
+        email
+      });
+      if (hash === md5(bodyToCheck)) {
+        Producer.update(
+          { confirmed: true },
+          { returning: true, plain: true, where: { business_name: user } }
+        );
+        console.log('CONFIRMED');
+      } else {
+        console.log('NOT CONFIRMED: ', hash, md5(bodyToCheck));
+      }
+    }
   } catch (err) {
     //eslint-disable-next-line
     console.error(err);
