df746c4f801baf2374c1ac6a5215a0123a235d7d
Marco Galizzi <marcogalizzi89@gmail.com>
2018-10-17 16:44:49 +0200

    fix: unsaved file

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -2,34 +2,6 @@ const keyPublishable = process.env.PUBLISHABLE_KEY;
 const keySecret = process.env.SECRET_KEY;
 const stripe = require('stripe')(keySecret);
 
-/* exports.charge = async ctx => {
-  console.log(ctx.request.body);
-  stripe.customers
-    .create({
-      card: ctx.request.body.card,
-      email: 'bob@bob.com'
-    })
-    .then(customer => {
-      console.log('customer: ', customer);
-      stripe.charges.create({
-        amount: 500,
-        description: 'test',
-        currency: 'usd',
-        customer: customer.id,
-        source: token.id
-      });
-    })
-    .then(charge => {
-      ctx.status = 200;
-      ctx.body = charge;
-    })
-    .catch(err => {
-      console.log('Error: ', err);
-      ctx.status = 500;
-      ctx.body = { error: 'Purchase failed' };
-    });
-}; */
-
 exports.charge = async ctx => {
   let res = await stripe.charges
     .create({
@@ -38,7 +10,6 @@ exports.charge = async ctx => {
       source: ctx.request.body.id
     })
     .then(function (charge) {
-      console.log('charge: ', charge);
       return charge;
     });
   ctx.body = res;
